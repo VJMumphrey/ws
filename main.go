3e0c0942bdb1388e324c76432c91354d3b026a65
VJMumphrey <vjm007@latech.edu>
2025-03-17 10:26:42 -0500

    quick and dirty fix

--- main.go
+++ main.go
@@ -10,6 +10,14 @@ import (
 
 	"github.com/chzyer/readline"
 	"github.com/spf13/cobra"
+
+	"crypto/tls"
+	"encoding/hex"
+	"net/http"
+	"regexp"
+
+	"github.com/fatih/color"
+	"github.com/gorilla/websocket"
 )
 
 const Version = "0.2.1"
@@ -17,7 +25,7 @@ const Version = "0.2.1"
 var options struct {
 	origin       string
 	printVersion bool
-	insecure bool
+	insecure     bool
 }
 
 func main() {
@@ -78,5 +86,90 @@ func root(cmd *cobra.Command, args []string) {
 		if err != io.EOF && err != readline.ErrInterrupt {
 			os.Exit(1)
 		}
+	}
+}
+
+type session struct {
+	ws      *websocket.Conn
+	rl      *readline.Instance
+	errChan chan error
+}
+
+func connect(url, origin string, rlConf *readline.Config, allowInsecure bool) error {
+	headers := make(http.Header)
+	headers.Add("Origin", origin)
+
+	dialer := websocket.Dialer{
+		Proxy: http.ProxyFromEnvironment,
+		TLSClientConfig: &tls.Config{
+			InsecureSkipVerify: allowInsecure,
+		},
+	}
+	ws, _, err := dialer.Dial(url, headers)
+	if err != nil {
+		return err
+	}
+
+	rl, err := readline.NewEx(rlConf)
+	if err != nil {
+		return err
+	}
+	defer rl.Close()
+
+	sess := &session{
+		ws:      ws,
+		rl:      rl,
+		errChan: make(chan error),
+	}
+
+	go sess.readConsole()
+	go sess.readWebsocket()
+
+	return <-sess.errChan
+}
+
+func (s *session) readConsole() {
+	for {
+		line, err := s.rl.Readline()
+		if err != nil {
+			s.errChan <- err
+			return
+		}
+
+		err = s.ws.WriteMessage(websocket.TextMessage, []byte(line))
+		if err != nil {
+			s.errChan <- err
+			return
+		}
+	}
+}
+
+func bytesToFormattedHex(bytes []byte) string {
+	text := hex.EncodeToString(bytes)
+	return regexp.MustCompile("(..)").ReplaceAllString(text, "$1 ")
+}
+
+func (s *session) readWebsocket() {
+	rxSprintf := color.New(color.FgGreen).SprintfFunc()
+
+	for {
+		msgType, buf, err := s.ws.ReadMessage()
+		if err != nil {
+			s.errChan <- err
+			return
+		}
+
+		var text string
+		switch msgType {
+		case websocket.TextMessage:
+			text = string(buf)
+		case websocket.BinaryMessage:
+			text = bytesToFormattedHex(buf)
+		default:
+			s.errChan <- fmt.Errorf("unknown websocket frame type: %d", msgType)
+			return
+		}
+
+		fmt.Fprint(s.rl.Stdout(), rxSprintf("< %s\n", text))
 	}
 }
